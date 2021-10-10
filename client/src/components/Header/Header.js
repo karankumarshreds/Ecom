d79683daf17957cc1ce7b96a15d58d5ed6a23f85
karankumarshreds <naileditnerd@gmail.com>
2021-10-10 11:19:11 +0530

    add header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import logo from '../../assets/logo.png';
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div className="wrapper">
+        <div className="logo">
+          <img style={{ width: 60 }} src={logo} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
